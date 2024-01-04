ba1345af9ac7aa72bdb21f176daf2597d0a80783
ahmadalsayeddd <ahmad.alsayed.asd@gmail.com>
2024-01-04 12:06:37 +0300

    first commit

---
+++ resources/views/dashboard/categories/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Create Categories')
+
+@section('breadcrumb')
+    @parent
+    <li class="breadcrumb-item active">Categories</li>
+    <li class="breadcrumb-item active">Create Category</li>
+
+@endsection
+
+@section('content')
+
+    <form action="{{ route('dashboard.categories.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        @include('dashboard.categories._form')
+    </form>
+
+@endsection
